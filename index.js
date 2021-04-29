0193b48a88e5a99641d16323aa65a6d854e89c9c
trak-dev <yann.picaud@gmail.com>
2021-04-29 14:49:22 +0200

    titre et plus

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-function getRandomImage(){
+function getRandomImage() {
   const randomImageApiUrl = "https://dog.ceo/api/breeds/image/random";
 
   // we are using fetch api to make rest api calls. you can use axios use.
-  // we are also using promises here. 
+  // we are also using promises here.
   fetch(randomImageApiUrl)
-    .then(function(response){
+    .then(function (response) {
       // we get raw response. need to first convert it into json format so we can use the data easily
       return response.json();
     })
-    .then(function(json){
-      // now we got the json . we can use this to update any data in html 
-      console.log(json);
+    .then(function (json) {
       var imageUrl = json.message;
-      var img = document.createElement('img');
-      img.src = imageUrl
-      document.getElementById('photos').appendChild(img);
+      var img = document.createElement("img");
+      img.src = imageUrl;
+      document.getElementById("photos").appendChild(img);
     })
-    .catch(function(error){
+    .catch(function (error) {
       // if any error occurs like no internet connection then this callback will be called
       console.log(error);
-      
     });
 }
-for (let i = 0 ; i <= 20 ; i++) {
+for (let i = 0; i <= 15; i++) {
   getRandomImage();
 }
